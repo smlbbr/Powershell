cf35b3867ae49bc4c662cfed9b561610acf2da6b
samuel <smlbbr@gmail.com>
2015-01-23 17:10:24 -0600

    createbulkvms

---
+++ CreateVirtualMachines.ps1
@@ -0,0 +1,47 @@
+﻿*************Bulk create generation 2 virtual machines**************
+
+$VMLocation = "E:\VMs"
+$VMISO = "E:\ISO\W2012R2.iso"
+$VMNetwork = "Internal"
+
+# Create DC01
+$VMName = "DC01"
+$VMMemory = 1024MB
+$VMDiskSize = 40GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+
+# Create CM01
+$VMName = "CM01"
+$VMMemory = 6144MB
+$VMDiskSize = 300GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+
+*************Bulk create generation 1 virtual machines**************
+
+$VMLocation = "E:\VMs"
+$VMISO = "E:\ISO\W2012R2.iso"
+$VMNetwork = "Internal"
+
+# Create DC01
+$VMName = "DC01"
+$VMMemory = 1024MB
+$VMDiskSize = 40GB
+New-VM -Name $VMName -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+
+# Create CM01
+$VMName = "CM01"
+$VMMemory = 6144MB
+$VMDiskSize = 300GB
+New-VM -Name $VMName -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
